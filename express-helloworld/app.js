1d76a0fb9c9ac694e20b6aefb27a60c5236125ae
linuxguru4ever <linuxguru4ever@gmail.com>
2025-08-29 07:25:00 -0400

    8/29 updates!

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,14 +1,37 @@
-var express = require('express');
-app = express();
+  var express = require('express');
+  app = express();
 
+// Define a route for the home page
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  // res.send('Hello World!\n');
+  res.send('Welcome to the Home page\n');
+});
+
+// About route
+app.get('/about', function(req, res) {
+  res.send('This is the About Page.');
+});
+
+// Contact route
+app.get('/contact', function(req, res) {
+  res.send('Contact us at contact@example.com');
+});
+
+// POST route example
+app.post('/submit', function(req, res) {
+  res.send('Form submitted successfully!');
+})
+
+// PUT route example
+app.put('/update', function(req, res) {
+  res.send('Update successful!');
 });
 
 app.get('/mars', function(req, res) {
   res.send('Hello Mars! Update-App!!!\n');
 });
 
+//start the server on port 8080
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
