fd8b667d6e712a0ec9d85c98c1201f092a39704d
linuxguru4ever <linuxguru4ever@gmail.com>
2025-08-29 07:57:58 -0400

    renamed the proj folder

---
+++ express-helloworld/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the HTML page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Start the server
+//app.listen(3000, () => {
+//  console.log('Server is running at http://localhost:4000');
+//});
+
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars! Update-App!!!\n');
+});
+
+//start the server on port 8080
+app.listen(8080, function () {
+  console.log('Example app listening on port 8088!');
+});
