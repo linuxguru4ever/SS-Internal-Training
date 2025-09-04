fad7468d1b0f0ee79a6f4974125be83a7027727c
linuxguru4ever <linuxguru4ever@gmail.com>
2025-09-04 10:27:34 -0400

    New commit 9/4 with Surendra

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mars', function(req, res) {
-  res.send('Hello Mars! Update-App!!!\n');
+  res.send('Hello Mars! Update-App!!! New update 9/4\n');
 });
 
 app.listen(8080, function () {
