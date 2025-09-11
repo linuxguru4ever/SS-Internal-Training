0cc3d648a8674fcc01910f740644fb3ad9d682a7
linuxguru4ever <linuxguru4ever@gmail.com>
2025-09-11 08:06:01 -0400

    sql fs names updated

--- contacts/sql/contacts.sql
+++ contacts/sql/contacts.sql
@@ -3,11 +3,11 @@ drop table if exists contacts;
 create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null);
 
 insert into contacts(firstname, lastname, email) values
-  ('Bilbo','Baggins','bilbo@theshire.com'),
-  ('Frodo','Baggins','frodo@theshire.com'),
-  ('Samwise','Gamgee','sam@theshire.com'),
-  ('Peregrin','Took','pippin@theshire.com'),
-  ('Meriadoc','Brandybuck','merry@theshire.com');
+  ('fname1','sname1','bilbo@testdomain.com'),
+  ('fname2','sname2','frodo@testdomain.com'),
+  ('fname3','sname3','sam@testdomain.com'),
+  ('fname4','sname4','pippin@testdomain.com'),
+  ('fname5','sname5','merry@testdomain.com');
 
 
 
